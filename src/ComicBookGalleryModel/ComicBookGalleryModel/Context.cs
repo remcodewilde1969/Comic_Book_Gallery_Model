748f8867f426d67043accba1aa6ce0c2e5521f7d
Remco de Wilde <remcow@vanbreda.nl>
2017-04-23 09:18:34 +0200

    Eerste versie zonder relatie database

---
+++ src/ComicBookGalleryModel/ComicBookGalleryModel/Context.cs
@@ -0,0 +1,26 @@
+﻿using ComicBookGalleryModel.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComicBookGalleryModel
+{
+    public class Context : DbContext
+    {
+        public Context()
+        {
+            //drop database als het model veranderd
+            Database.SetInitializer(new DropCreateDatabaseIfModelChanges<Context>());
+            
+            //EF gebruikt create datebase if not exists by default dus eigenlijk niet nodig
+            //Database.SetInitializer(new CreateDatabaseIfNotExists<Context>());
+
+            //drop database bij elke herstart
+            //Database.SetInitializer(new DropCreateDatabaseAlways<Context>());
+        }
+        public DbSet<ComicBook> ComicBooks { get; set; }
+    }
+}
---
+++ src/ComicBookGalleryModel/ComicBookGalleryModel/Program.cs
@@ -0,0 +1,33 @@
+﻿using ComicBookGalleryModel.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComicBookGalleryModel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var context = new Context())
+            {
+                context.ComicBooks.Add(new ComicBook()
+                    {
+                        SeriesTitle="Book naam demo",
+                        IssueNumber=1,
+                        PublishedOn=DateTime.Today
+                    });
+                context.SaveChanges();
+
+                var comicBooks = context.ComicBooks.ToList();
+                foreach(var comicBook in comicBooks)
+                {
+                    Console.WriteLine(comicBook.SeriesTitle);
+                }
+                Console.ReadLine();
+            }
+        }
+    }
+}
