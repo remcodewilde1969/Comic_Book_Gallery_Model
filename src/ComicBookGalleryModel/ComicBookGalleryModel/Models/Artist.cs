0b11cffcea24c499566e7368e2ebb0acea673630
Remco de Wilde <remcow@vanbreda.nl>
2017-04-23 14:17:07 +0200

    Aanpassen max waarde tabellen en wel of niet null

--- src/ComicBookGalleryModel/ComicBookGalleryModel/Models/Artist.cs
+++ src/ComicBookGalleryModel/ComicBookGalleryModel/Models/Artist.cs
@@ -1,19 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ComicBookGalleryModel.Models
 {
+    [Table("Talent")] 
+    //tabel zal niet Artist worden maar Talent
     public class Artist
     {
+        
         public Artist()
         {
             ComicBooks = new List<ComicBookArtist>();
         }
         public int Id { get; set; }
+        [Required, StringLength(100), Column ("FullName")]
+        //zorgt er voor dat null niet meer mag en max 100 karakters
+        // en dat de naam van de Kolom niet Name maar FullName wordt EF vertaalt dit weer naar Name
         public string Name { get; set; }
+
+        [NotMapped] 
+        // prop wordt geen tabel in DB  
+        public string Test { get; set; }
 
 
         public ICollection<ComicBookArtist> ComicBooks { get; set; }
--- src/ComicBookGalleryModel/ComicBookGalleryModel/Models/Series.cs
+++ src/ComicBookGalleryModel/ComicBookGalleryModel/Models/Series.cs
@@ -16,6 +16,7 @@ namespace ComicBookGalleryModel.Models
         }
 
         public int Id { get; set; }
+
         public string Title { get; set; }
         public string Description { get; set; }
 
