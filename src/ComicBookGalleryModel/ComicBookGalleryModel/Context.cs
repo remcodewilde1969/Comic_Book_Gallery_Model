133d5a0560cf249920ea82344b52d9df14cb5519
Remco de Wilde <remcow@vanbreda.nl>
2017-04-23 14:35:43 +0200

    Aanpassen database init in Context

--- src/ComicBookGalleryModel/ComicBookGalleryModel/Context.cs
+++ src/ComicBookGalleryModel/ComicBookGalleryModel/Context.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -22,5 +23,27 @@ namespace ComicBookGalleryModel
             Database.SetInitializer(new DropCreateDatabaseAlways<Context>());
         }
         public DbSet<ComicBook> ComicBooks { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            //base.OnModelCreating(modelBuilder);
+
+            // remove pluralizing
+
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+
+            // pas default waarde van bijvoorbeeld decimal grade aan 
+            // 5, 2 geeft XXX.XX
+
+            // onderstaande zal prima werken maar voor elke decimal die je maakt.
+            //modelBuilder.Conventions.Remove<DecimalPropertyConvention>();
+            //modelBuilder.Conventions.Add(new DecimalPropertyConvention(5, 2));
+
+            //Beter is het op de entity zelf te doen
+
+            modelBuilder.Entity<ComicBook>()
+                .Property(cb => cb.AverageRating)
+                .HasPrecision(5, 2);
+        }
     }
 }
